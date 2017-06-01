b76eb37c0a01588018014d1e8735ccd12f62bd8a
Marcus Hunt <marcus.hunt@MVFLP0422.local>
2017-06-01 17:27:37 +0100

    Added output .md file

--- feedsreader/reader.py
+++ feedsreader/reader.py
@@ -7,16 +7,31 @@ import ssl
 import urllib.request
 import feedparser
 
+print("Working...")
+
 #http://stackoverflow.com/questions/27835619/ssl-certificate-verify-failed-error
 ssl._create_default_https_context = ssl._create_unverified_context
 
 with urllib.request.urlopen(url) as response:
     html = response.read()
 
-for line in html.splitlines():
-    url = line.decode("utf-8")
-    #Get RSS for each URL.
-    d = feedparser.parse(url)
-    #Extract latest story.
-    print(url)
-    print(d.entries[0].title)
+with open('output/blogroll.md', 'w') as output:
+    #output.write("hello markdown file")
+    output.write("Title: Blogroll")
+    output.write("\n")
+    for line in html.splitlines():
+        url = line.decode("utf-8")
+        #Get RSS for each URL.
+        d = feedparser.parse(url)
+        #Extract latest story.
+        #output.write("\n")
+        #output.write(url)
+        output.write("[" + d.entries[0].title + "]")
+        output.write("(" + d.entries[0].link + ")")
+        output.write("\n")
+        output.write(d.entries[0].description[0:100])
+        #output.write("\n")
+        output.write("\n")
+    output.close
+
+print(output.closed)
