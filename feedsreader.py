e72819e143b8287796e3ce25b5733a474d522da7
Marcus Hunt <marcus.hunt@MVFLP0422.local>
2017-06-16 13:54:56 +0100

    Use urls.txt file.

--- feedsreader.py
+++ feedsreader.py
@@ -7,7 +7,7 @@ import feedparser
 
 def executeFeedReadWrite():
     #Extract URLs from public dropbox file.
-    url = "http://www.dropbox.com/s/yuo52behpqchn39/TechFeedURLs.txt?dl=1"  # dl=1 is important
+    url = "https://gitlab.com/mcxvio/feeds/blob/master/public/urls.txt"  # dl=1 is important
 
     #http://stackoverflow.com/questions/27835619/ssl-certificate-verify-failed-error
     ssl._create_default_https_context = ssl._create_unverified_context
